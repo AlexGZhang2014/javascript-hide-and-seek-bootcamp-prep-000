df542cd3a7f890dd18462275f4ed8c2d8c9529a5
Alex Zhang <alexgzhang92@gmail.com>
2018-05-13 01:49:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,38 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  let current = div#grand-node;
-  let next = [];
+  let current = document.querySelector('div#grand-node');
+  let next = []
+ 
+  // hey, a `while` loop! this loop will only
+  // trigger if `current` is truthy — so when
+  // we exhaust `next` and, below, attempt to
+  // `shift()` `undefined` (when `next` is empty)
+  // onto `current`, we'll exit the loop
   while (current) {
-    
+    // if `current` satisfies the `criteriaFn`, then
+    // return it — recall that `return` will exit the
+    // entire function!
+    if (criteriaFn(current)) {
+      return current
+    }
+ 
+    // if `current` is an array, we want to push all of
+    // its elements (which might be arrays) onto `next`
+    if (Array.isArray(current)) {
+      for (let i = 0; i < current.length; i++) {
+        next.push(current[i])
+      }
+    }
+ 
+    // after pushing any children (if there
+    // are any) of `current` onto `next`, we want to take
+    // the first element of `next` and make it the
+    // new `current` for the next pass of the `while`
+    // loop
+    current = next.shift()
   }
+ 
+  // if we haven't
+  return null
 }
