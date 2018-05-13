ed4d528df408e641db2f4a7b9814d03744c78297
Alex Zhang <alexgzhang92@gmail.com>
2018-05-13 01:47:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  let number = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < number.length; i++) {
+    number[i].innerHTML = parseInt(number[i].innerHTML) + n;
+  }
+  return number;
+}
+
+function deepestChild() {
+  let current = div#grand-node;
+  let next = [];
+  while (current) {
+    
+  }
+}
